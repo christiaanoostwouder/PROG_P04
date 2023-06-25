c8d03943ba59de3c07befe2023a6e6ddc7a2a739
christiaan Oostwouder <36211@ma-web.nl>
2023-06-25 11:02:32 +0200

    enum 1

---
+++ Enum/EnumOpdracht/EnumOpdracht/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOpdracht
+{
+    internal enum Colors
+    {
+        blue,
+        purple,
+        green,
+        lime,
+
+
+    }
+}
---
+++ Enum/EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Colors myMainThemeColors = Colors.purple;
+            if(myMainThemeColors == Colors.purple)
+            {
+                Console.WriteLine("dat is mijn kleur");
+            }
+        }
+    }
+
+
+}
