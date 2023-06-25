962ecd9e80b47652b0d44f117f8daa313d0bf81a
christiaan Oostwouder <36211@ma-web.nl>
2023-06-25 11:42:41 +0200

    filesDir

---
+++ Files en Directories/FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FileDirOpdracht
+{
+    internal class Pragram
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\oostw\OneDrive\Bureaublad\MA college\periode 4\PROG\PROG_P04");
+            FileInfo[] files = dir.GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                Console.WriteLine(dirs[i]);
+            }
+        }
+    }
+}
